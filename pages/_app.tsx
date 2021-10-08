fefa7ef2c32df5f0139618531db9be03d7c7400c
Ilya Motamedi <ilya.motamedi@gmail.com>
2021-10-08 18:49:07 -0400

    added TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import type { AppProps } from "next/app";
 
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
